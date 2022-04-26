fa39af284bf2ab8b179d3d7504e7ee000b525e7b
GalaX1us <75265945+GalaX1us@users.noreply.github.com>
2022-04-26 17:46:12 +0200

    Add files via upload

---
+++ Codes Produit/generateur.py
@@ -0,0 +1,7 @@
+nb = int(input("Combien voulez vous de séquences :"))
+alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+cpt=0
+with open("resultat.txt", "w") as fillout:
+    while cpt<nb:
+        fillout.write("{}{}{}{}{}{}\n".format(alphabet[(cpt//26**5)%26],alphabet[(cpt//26**4)%26],alphabet[(cpt//26**3)%26],alphabet[(cpt//26**2)%26],alphabet[(cpt//26)%26],alphabet[(cpt%26)]))
+        cpt+=1
