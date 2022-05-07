d534263be41cd188757e8abc845bc8ed4ef64bc4
Nathan BONADA <75265945+GalaX1us@users.noreply.github.com>
2022-05-07 22:08:21 +0200

    Update generateur.py

--- Codes Produit/generateur.py
+++ Codes Produit/generateur.py
@@ -1,4 +1,4 @@
-nb = int(input("Combien voulez vous de séquences :"))
+nb = int(input("Combien voulez vous de code :"))
 alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 cpt=0
 with open("resultat.txt", "w") as fillout:
